54d77b1c1d0b1e5995f4f0837fb5bb53b932d8ea
AngaBlue <contact@anga.blue>
2022-08-08 20:28:29 +1000

    Linting

--- src/components/messages/dental/data.tsx
+++ src/components/messages/dental/data.tsx
@@ -1,40 +1,30 @@
 // Technic as Techniek
-export const Technic = [
-  'Implantaat',
-  'Brug',
-  'Sinuslift',
-  'All-on',
-  'Wortelkanaal behandeling',
-]
+export const Technic = ['Implantaat', 'Brug', 'Sinuslift', 'All-on', 'Wortelkanaal behandeling'];
 // Implant as Im plantaat
-export const ImplantOptions = ['A-Kwaliteit €895', 'B-Kwaliteit €695']
-export const ImplantMaterials = [
-  'Porselein €195',
-  'Zirconium €295',
-  'E-Maz €315',
-]
+export const ImplantOptions = ['A-Kwaliteit €895', 'B-Kwaliteit €695'];
+export const ImplantMaterials = ['Porselein €195', 'Zirconium €295', 'E-Maz €315'];
 // Brug
 export const BrugOptions = [
-  'Driedelige brug   3x material-crown',
-  'Vierdelige brug   4x material-crown',
-  'Vijfdelige brug   5x material-crown',
-]
-export const BrugMaterials = ['Porselein €195', 'Zirconium €295', 'E-Maz €315']
+    'Driedelige brug   3x material-crown',
+    'Vierdelige brug   4x material-crown',
+    'Vijfdelige brug   5x material-crown'
+];
+export const BrugMaterials = ['Porselein €195', 'Zirconium €295', 'E-Maz €315'];
 // AllOn
-export const AllOn = ['All on 4', 'All on 6']
+export const AllOn = ['All on 4', 'All on 6'];
 // Wortel
-export const Wortel = ['1   €125', '2   €165', '3     €195']
+export const Wortel = ['1   €125', '2   €165', '3     €195'];
 // SinusLift
-export const SinusLift = ['Open €1155', 'Gesloten €750']
+export const SinusLift = ['Open €1155', 'Gesloten €750'];
 
-//defaultState for DentalState
+// defaultState for DentalState
 export const defaultState = {
-  firstname: '',
-  lastname: '',
-  email: '',
-  // date: '',
-  technic: Technic[0],
-  treatments: '',
-}
+    firstname: '',
+    lastname: '',
+    email: '',
+    // date: '',
+    technic: Technic[0],
+    treatments: ''
+};
 
-export type DentalState = typeof defaultState
+export type DentalState = typeof defaultState;
--- src/components/messages/dental/form.tsx
+++ src/components/messages/dental/form.tsx
@@ -1,87 +1,59 @@
 import {
-  AllOn,
-  BrugMaterials,
-  BrugOptions,
-  DentalState,
-  ImplantOptions,
-  SinusLift,
-  Technic,
-  Wortel,
-} from './data'
-import {
-  Box,
-  Checkbox,
-  FormLabel,
-  NumberDecrementStepper,
-  NumberIncrementStepper,
-  NumberInput,
-  NumberInputField,
-  NumberInputStepper,
-  Select,
-  SimpleGrid,
-  Text,
-  Textarea,
-} from '@chakra-ui/react'
-import { Dispatch, SetStateAction, useCallback, useState } from 'react'
+    Box,
+    Checkbox,
+    FormLabel,
+    NumberDecrementStepper,
+    NumberIncrementStepper,
+    NumberInput,
+    NumberInputField,
+    NumberInputStepper,
+    Select,
+    SimpleGrid
+} from '@chakra-ui/react';
+import { ChangeEventHandler, Dispatch, SetStateAction } from 'react';
+import { DentalState, Technic } from './data';
+import styles from './content.module.scss';
 
-import styles from './content.module.scss'
+export default function Form({ state, setState }: { state: DentalState; setState: Dispatch<SetStateAction<DentalState>> }) {
+    const changeTechniken: ChangeEventHandler<HTMLSelectElement> = e => setState({ ...state, technic: e.target.value });
 
-export default function Form({
-  state,
-  setState,
-}: {
-  state: DentalState
-  setState: Dispatch<SetStateAction<DentalState>>
-}) {
-  const ChangeTechniken = useCallback(
-    (e: any) => {
-      setState({ ...state, technic: e.target.value })
-    },
-    [state.technic],
-  )
+    // const ChangeTechniken = (e: any) => {
+    //   setState({ ...state, technic: e.target.value })
+    //   console.log('>>>', state.technic)
+    // }
+    return (
+        <div>
+            <SimpleGrid columns={[1, null, 2, 3, 4]} spacing={4}>
+                <Box>
+                    <FormLabel>Techniek</FormLabel>
+                    <Select value={state.technic} onChange={changeTechniken}>
+                        {Technic.map(v => (
+                            <option key={v} value={v}>
+                                {v}
+                            </option>
+                        ))}
+                    </Select>
 
-  // const ChangeTechniken = (e: any) => {
-  //   setState({ ...state, technic: e.target.value })
-  //   console.log('>>>', state.technic)
-  // }
-  return (
-    <div>
-      <SimpleGrid columns={[1, null, 2, 3, 4]} spacing={4}>
-        <Box>
-          <FormLabel>Techniek</FormLabel>
-          <Select
-            value={state.technic}
-            onChange={(e) => {
-              ChangeTechniken(e)
-            }}
-          >
-            {Technic.map((v) => (
-              <option key={v} value={v}>
-                {v}
-              </option>
-            ))}
-          </Select>
+                    <FormLabel mt={6}>Bone graft</FormLabel>
+                    <Checkbox ml={3} size='sm' colorScheme='green' value=''>
+                        € 275
+                    </Checkbox>
+                </Box>
 
-          <FormLabel mt={6}>Bone graft</FormLabel>
-          <Checkbox ml={3} size="sm" colorScheme="green" value="">
-            € 275
-          </Checkbox>
-        </Box>
-
-        <Box>
-          <FormLabel>Extractie</FormLabel>
-          <NumberInput min={1} max={32} step={1} precision={0}>
-            <NumberInputField />
-            <NumberInputStepper>
-              <NumberIncrementStepper />
-              <NumberDecrementStepper />
-            </NumberInputStepper>
-          </NumberInput>
-        </Box>
-      </SimpleGrid>
-      <div className={styles.tooth_content}>
-        <div className={styles.tooth_buttons}></div>
-      </div>
-    </div>
-  )
+                <Box>
+                    <FormLabel>Extractie</FormLabel>
+                    <NumberInput min={1} max={32} step={1} precision={0}>
+                        <NumberInputField />
+                        <NumberInputStepper>
+                            <NumberIncrementStepper />
+                            <NumberDecrementStepper />
+                        </NumberInputStepper>
+                    </NumberInput>
+                </Box>
+            </SimpleGrid>
+            <div className={styles.tooth_content}>
+                <div className={styles.tooth_buttons}></div>
+            </div>
+        </div>
+    );
 }
