6b2671af71c51c9f6eadd514852e32295ad60cdc
WebCaesar <jin.dong.caesar@gmail.com>
2022-08-10 03:02:28 -0500

    WorkedForImplant

--- src/components/messages/dental/form.tsx
+++ src/components/messages/dental/form.tsx
@@ -1,79 +1,217 @@
 // Chakra and React Hook
 import {
-    Box,
-    FormLabel,
-    NumberDecrementStepper,
-    NumberIncrementStepper,
-    NumberInput,
-    NumberInputField,
-    NumberInputStepper,
-    Select,
-    SimpleGrid
-} from '@chakra-ui/react';
-import { ChangeEventHandler, Dispatch, SetStateAction, useCallback } from 'react';
+  Box,
+  FormLabel,
+  Input,
+  InputGroup,
+  NumberDecrementStepper,
+  NumberIncrementStepper,
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
+  Radio,
+  RadioGroup,
+  Select,
+  SimpleGrid,
+  VStack,
+} from '@chakra-ui/react'
+import {
+  ChangeEventHandler,
+  Dispatch,
+  SetStateAction,
+  useCallback,
+} from 'react'
 // Import Data
-import { DentalState, Technic } from './data';
+import { DentalState, Gender, Technic } from './data'
 
 // Import Techniek Components
-import AllOnCom from './technics/AllOnCom';
-import BrugCom from './technics/BrugCom';
-import ImplantaatCom from './technics/ImplantaatCom';
-import SinusliftCom from './technics/SinusliftCom';
-import WortelkanaalCom from './technics/WortelkanaalCom';
+import AllOnCom from './technics/AllOnCom'
+import BrugCom from './technics/BrugCom'
+import DatePicker from 'react-datepicker'
+import ImplantaatCom from './technics/ImplantaatCom'
+import SinusliftCom from './technics/SinusliftCom'
+import WortelkanaalCom from './technics/WortelkanaalCom'
 // Styles
-import styles from './content.module.scss';
+import styles from './content.module.scss'
 
 // ** Main **
-export default function Form({ state, setState }: { state: DentalState; setState: Dispatch<SetStateAction<DentalState>> }) {
-    // ChangeHandler Techniek in Selector
-    const changeTechniken: ChangeEventHandler<HTMLSelectElement> = e => setState({ ...state, technic: e.target.value });
-    // Select Componentin Selector
-    const SelectTechniek = useCallback(() => {
-        switch (state.technic) {
-            case 'Implantaat':
-                return <ImplantaatCom state={state} setState={setState} />;
-            case 'Brug':
-                return <BrugCom state={state} setState={setState} />;
-            case 'Sinuslift':
-                return <SinusliftCom state={state} setState={setState} />;
-            case 'All-on':
-                return <AllOnCom state={state} setState={setState} />;
-            case 'Wortelkanaal behandeling':
-                return <WortelkanaalCom state={state} setState={setState} />;
-            default:
-                break;
-        }
-    }, [state, setState]);
-    console.log('@@@', state.technic);
-
-    return (
-        <div>
-            <SimpleGrid columns={[1, null, 2, 3, 4]} spacing={4}>
-                <Box>
-                    <FormLabel>Techniek</FormLabel>
-                    <Select value={state.technic} onChange={changeTechniken}>
-                        {Technic.map(v => (
-                            <option key={v} value={v}>
-                                {v}
-                            </option>
-                        ))}
-                    </Select>
-                    <>{SelectTechniek()}</>
-                </Box>
-                <Box>
-                    <FormLabel>Extractie</FormLabel>
-                    <NumberInput min={1} max={32} step={1} precision={0}>
-                        <NumberInputField />
-                        <NumberInputStepper>
-                            <NumberIncrementStepper />
-                            <NumberDecrementStepper />
-                        </NumberInputStepper>
-                    </NumberInput>
-                </Box>
-            </SimpleGrid>
-            <div className={styles.tooth_content}>
-                <div className={styles.tooth_buttons}></div>
+export default function Form({
+  state,
+  setState,
+}: {
+  state: DentalState
+  setState: Dispatch<SetStateAction<DentalState>>
+}) {
+  // ChangeHandler Techniek in Selector
+  const changeTechniken: ChangeEventHandler<HTMLSelectElement> = (e) =>
+    setState({ ...state, technic: e.target.value })
+  // Select Componentin Selector
+  const SelectTechniek = useCallback(() => {
+    switch (state.technic) {
+      case 'Implantaat':
+        return <ImplantaatCom state={state} setState={setState} />
+        break
+      case 'Brug':
+        return <BrugCom state={state} setState={setState} />
+        break
+      case 'Sinuslift':
+        return <SinusliftCom state={state} setState={setState} />
+        break
+      case 'All-on':
+        return <AllOnCom state={state} setState={setState} />
+        break
+      case 'Wortelkanaal behandeling':
+        return <WortelkanaalCom state={state} setState={setState} />
+        break
+      default:
+        break
+    }
+  }, [state.technic])
+  return (
+    <div>
+      <SimpleGrid columns={[1, null, 2, 3, 4]} spacing={4}>
+        <Box>
+          <FormLabel>Voornaam Klant</FormLabel>
+          <InputGroup>
+            <Input
+              placeholder="John"
+              value={state.firstname}
+              onChange={(e) =>
+                setState({ ...state, firstname: e.target.value })
+              }
+            />
+          </InputGroup>
+        </Box>
+        <Box>
+          <FormLabel>Achternaam Klant</FormLabel>
+          <InputGroup>
+            <Input
+              placeholder="Smith"
+              value={state.lastname}
+              onChange={(e) => setState({ ...state, lastname: e.target.value })}
+            />
+          </InputGroup>
+        </Box>
+        <Box>
+          <FormLabel>Geslacht</FormLabel>
+          <RadioGroup
+            value={state.gender}
+            onChange={(gender) => setState({ ...state, gender })}
+          >
+            <VStack align="left">
+              {Object.values(Gender).map((c) => (
+                <Radio key={c} value={c}>
+                  {c}
+                </Radio>
+              ))}
+            </VStack>
+          </RadioGroup>
+        </Box>
+        <Box>
+          <FormLabel>Datum</FormLabel>
+          <InputGroup className={styles.datepicker}>
+            <DatePicker
+              selected={new Date(state.date)}
+              onChange={(e) =>
+                setState({
+                  ...state,
+                  date: e instanceof Date ? e.getTime() : Date.now(),
+                })
+              }
+              dateFormat="PPP"
+            />
+          </InputGroup>
+        </Box>
+        <Box>
+          <FormLabel>Techniek</FormLabel>
+          <Select value={state.technic} onChange={changeTechniken}>
+            {Technic.map((v, key) => (
+              <option key={v} value={v}>
+                {v}
+              </option>
+            ))}
+          </Select>
+          <>{SelectTechniek()}</>
+        </Box>
+        <Box>
+          <FormLabel>Extractie</FormLabel>
+          <NumberInput min={1} max={32} step={1} precision={0}>
+            <NumberInputField />
+            <NumberInputStepper>
+              <NumberIncrementStepper />
+              <NumberDecrementStepper />
+            </NumberInputStepper>
+          </NumberInput>
+        </Box>
+      </SimpleGrid>
+      <div className={styles.tooth_content}>
+        <div className={styles.tooth_buttons_top}>
+          {state.topTooth.map((topT, index) => (
+            <div
+              onClick={() => {
+                let TopTeeth = state.topTooth.map((item, count) =>
+                  count === index
+                    ? { ...item, clickNum: item.clickNum + 1 }
+                    : item,
+                )
+                // let TopTeeth = state.topTooth
+                // let TeethObj = TopTeeth[index]
+                // TeethObj.clickNum++
+                // TopTeeth[index] = TeethObj
+                setState({
+                  ...state,
+                  topTooth: TopTeeth,
+                })
+              }}
+              className={styles.teeth_btn_top}
+              style={{ width: topT.teethWidth }}
+            >
+              {topT.clickNum % 2 == 1 && topT.clickNum > 0 ? (
+                <span className={styles.implantaat_image_top}>
+                  <img
+                    src="/topImplant.png"
+                    alt="Fue Perfect Banner"
+                    style={{ height: topT.teethHeight }}
+                  />
+                </span>
+              ) : null}
             </div>
+          ))}
         </div>
-    );
+
+        <div className={styles.tooth_buttons_bottom}>
+          {state.bottomTooth.map((bottomT, index) => (
+            <div
+              onClick={() => {
+                // let TopTeeth = state.topTooth
+                // let TeethObj = TopTeeth[index]
+                // TeethObj.clickNum++
+                // TopTeeth[index] = TeethObj
+                setState({
+                  ...state,
+                  bottomTooth: state.bottomTooth.map((item, count) =>
+                    count === index
+                      ? { ...item, clickNum: item.clickNum + 1 }
+                      : item,
+                  ),
+                })
+              }}
+              className={styles.teeth_btn_bottom}
+              style={{ width: bottomT.teethWidth }}
+            >
+              {bottomT.clickNum % 2 == 1 && bottomT.clickNum > 0 ? (
+                <span className={styles.implantaat_image_bottom}>
+                  <img
+                    src="/implantImg.png"
+                    alt="Fue Perfect Banner"
+                    style={{ height: bottomT.teethHeight }}
+                  />
+                </span>
+              ) : null}
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  )
 }
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,39 +1,62 @@
-import { Box, Center, Heading, Text, Flex, useColorMode } from '@chakra-ui/react';
-import Image from 'next/image';
-import NextLink from 'next/link';
-import { useRouter } from 'next/router';
-import { useEffect } from 'react';
-import GoogleButton from 'react-google-button';
-import { SEO } from '../components/SEO';
+import {
+  Box,
+  Center,
+  Flex,
+  Heading,
+  Text,
+  useColorMode,
+} from '@chakra-ui/react'
+
+import GoogleButton from 'react-google-button'
+import Image from 'next/image'
+import NextLink from 'next/link'
+import { SEO } from '../components/SEO'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 
 export default function Index() {
-    const router = useRouter();
-    const colorMode = useColorMode();
+  const router = useRouter()
+  const colorMode = useColorMode()
 
-    useEffect(() => {
-        if (router.query.navigate) router.push(router.query.navigate as string);
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [router.query]);
+  useEffect(() => {
+    if (router.query.navigate) router.push(router.query.navigate as string)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [router.query])
 
-    return (
-        <>
-            <SEO />
-            <Flex p={4} pt={2} height='100vh' w='full' alignItems='center' justifyContent='center' direction='column'>
-                <Box>
-                    <Box>
-                        <Center>
-                            <Image src='/banner.png' alt='Fue Perfect Banner' height={100} width={313} />
-                        </Center>
-                    </Box>
-                    <Heading textAlign='center' my={8}>
-                        Fue Perfect Email App
-                    </Heading>
-                    <Text mb={8}>Gebruikers op de witte lijst kunnen inloggen via google.</Text>
-                </Box>
-                <NextLink href='/api/google' passHref>
-                    <GoogleButton type={colorMode.colorMode} />
-                </NextLink>
-            </Flex>
-        </>
-    );
+  return (
+    <>
+      <SEO />
+      <Flex
+        p={4}
+        pt={2}
+        height="100vh"
+        w="full"
+        alignItems="center"
+        justifyContent="center"
+        direction="column"
+      >
+        <Box>
+          <Box>
+            <Center>
+              <Image
+                src="/banner.png"
+                alt="Fue Perfect Banner"
+                height={100}
+                width={313}
+              />
+            </Center>
+          </Box>
+          <Heading textAlign="center" my={8}>
+            Fue Perfect Email App
+          </Heading>
+          <Text mb={8}>
+            Gebruikers op de witte lijst kunnen inloggen via google.
+          </Text>
+        </Box>
+        <NextLink href="/api/google" passHref>
+          <GoogleButton type={colorMode.colorMode} />
+        </NextLink>
+      </Flex>
+    </>
+  )
 }
