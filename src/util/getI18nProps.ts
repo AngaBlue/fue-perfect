c2a1963d25e9fa1b7074499f04113a8e0dd4406f
AngaBlue <contact@anga.blue>
2022-09-13 00:38:52 +1000

    Dynamically load languages

--- src/util/getI18nProps.ts
+++ src/util/getI18nProps.ts
@@ -1,11 +1,11 @@
 import { GetStaticProps } from 'next';
 import { Locales } from '../i18n/i18n-types';
 import { baseLocale, loadedLocales } from '../i18n/i18n-util';
-import { loadAllLocalesAsync } from '../i18n/i18n-util.async';
+import { loadLocaleAsync } from '../i18n/i18n-util.async';
 
 const getI18nProps: GetStaticProps = async context => {
     const locale = (context.locale as Locales) ?? baseLocale;
-    await loadAllLocalesAsync();
+    await loadLocaleAsync(locale);
 
     return {
         props: {
