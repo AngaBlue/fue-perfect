a68c6953e783f51033e7f624849939c19ccc4d5a
Mitchell Reid <mitchellreid02@gmail.com>
2023-08-30 13:54:53 +1000

    Comment out erroring unused components

--- src/components/messages/dental/techniques/BrugCom.tsx
+++ src/components/messages/dental/techniques/BrugCom.tsx
@@ -3,27 +3,27 @@ import { ChangeEventHandler, Dispatch, SetStateAction } from 'react';
 import { BrugMaterials, BrugOptions, DentalState } from '../data';
 
 export default function BrugCom({ state, setState }: { state: DentalState; setState: Dispatch<SetStateAction<DentalState>> }) {
-    const changeBrugOptions: ChangeEventHandler<HTMLSelectElement> = e => setState({ ...state, brugOptions: e.target.value });
-    const changeBrugMaterials: ChangeEventHandler<HTMLSelectElement> = e => setState({ ...state, brugMaterials: e.target.value });
+    // const changeBrugOptions: ChangeEventHandler<HTMLSelectElement> = e => setState({ ...state, brugOptions: e.target.value });
+    // const changeBrugMaterials: ChangeEventHandler<HTMLSelectElement> = e => setState({ ...state, brugMaterials: e.target.value });
 
-    return (
-        <Box>
-            <FormLabel mt={6}>Opties</FormLabel>
-            <Select mt={6} onChange={changeBrugOptions} value={state.brugOptions}>
-                {BrugOptions.map(v => (
-                    <option key={v} value={v}>
-                        {v}
-                    </option>
-                ))}
-            </Select>
-            <FormLabel mt={6}>Material</FormLabel>
-            <Select onChange={changeBrugMaterials} value={state.brugMaterials}>
-                {BrugMaterials.map(v => (
-                    <option key={v} value={v}>
-                        {v}
-                    </option>
-                ))}
-            </Select>
-        </Box>
-    );
+    // return (
+    //     <Box>
+    //         <FormLabel mt={6}>Opties</FormLabel>
+    //         <Select mt={6} onChange={changeBrugOptions} value={state.brugOptions}>
+    //             {BrugOptions.map(v => (
+    //                 <option key={v} value={v}>
+    //                     {v}
+    //                 </option>
+    //             ))}
+    //         </Select>
+    //         <FormLabel mt={6}>Material</FormLabel>
+    //         <Select onChange={changeBrugMaterials} value={state.brugMaterials}>
+    //             {BrugMaterials.map(v => (
+    //                 <option key={v} value={v}>
+    //                     {v}
+    //                 </option>
+    //             ))}
+    //         </Select>
+    //     </Box>
+    // );
 }
