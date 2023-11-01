849a6b5ec96e75fc5ff499a62a4840b51d57ca12
Mitchell Reid <mitchellreid02@gmail.com>
2023-11-02 01:56:30 +1100

    Revert "Suppress hydration warning"
    This reverts commit 2c9a18840ca39b1117b487752e3505da7044002f.

--- src/components/messages/hair/content.tsx
+++ src/components/messages/hair/content.tsx
@@ -51,7 +51,7 @@ export default function Content({ state }: { state: HairState }) {
                 </>
             ) : (
                 <>
-                    <p suppressHydrationWarning>
+                    <p>
                         <BoldTranslation>
                             {LL.HAIR.CONTENT.NO_INSPECTION_1(new Date(state.date), LL.HAIR.TECHNIQUE[state.technique]())}
                         </BoldTranslation>
