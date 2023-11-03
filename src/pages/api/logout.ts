9d2e6e7c20766f5e1f7db5271d40531854aa33f6
Mitchell Reid <mitchellreid02@gmail.com>
2023-11-03 12:29:43 +1100

    Properly delete authentication cookie

--- src/pages/api/logout.ts
+++ src/pages/api/logout.ts
@@ -1,9 +1,15 @@
 import { NextApiHandler } from 'next';
 
+const isDev = process.env.NODE_ENV !== 'production';
+
 // eslint-disable-next-line consistent-return
 const logout: NextApiHandler = async (req, res): Promise<void> => {
     if (req.method === 'GET') {
-        res.setHeader('set-cookie', ['fp-google=deleted', 'Path=/', 'Max-Age=0', 'SameSite=Strict', 'HttpOnly'].join(';'));
+        const cookie = ['fp-google=deleted', 'Path=/', 'Max-Age=0', 'SameSite=Strict', 'HttpOnly'];
+
+        if (!isDev) cookie.push('Secure', `Domain=${req.headers.host}`);
+
+        res.setHeader('set-cookie', cookie.join(';'));
         res.redirect('/login?navigate=/');
         // eslint-disable-next-line consistent-return
         return;
